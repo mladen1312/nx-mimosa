d0263a5b47dd5ef0501fb8411203237489ed0937
Mladen Mester <61121560+mladen1312@users.noreply.github.com>
2026-02-03 05:51:04 +0100

    PRO v3.0: Update __init__.py

--- python/qedmma_pro/core/__init__.py
+++ python/qedmma_pro/core/__init__.py
@@ -1,4 +1,1 @@
-"""QEDMMA-Pro Core Algorithms"""
-from .ukf_pro import UKFPro, UKFProParams, UKFProState, create_hypersonic_ukf
-
-__all__ = ["UKFPro", "UKFProParams", "UKFProState", "create_hypersonic_ukf"]
+"""QEDMMA-PRO Core"""
